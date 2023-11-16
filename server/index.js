df1f87b6a0e66a4536c35714951332981ccc78b7
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-11-16 23:14:33 +0600

    update

--- server/index.js
+++ server/index.js
@@ -1,21 +1,42 @@
 /* eslint-disable no-undef */
-const express = require('express'); 
-const cors = require('cors'); 
-require('dotenv').config(); 
-  
-const app = express(); 
-const PORT = process.env.PORT; 
-const KEY = process.env.KEY; 
-app.use(express.json()); 
-app.use(cors()); 
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
-const url = `https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=${KEY}`;
-console.log(url);
+const app = express();
+const PORT = process.env.PORT;
+const KEY = process.env.KEY;
+app.use(express.json());
+app.use(cors());
 
-app.listen(PORT, () =>{
- app.get("/", async (req, res) => {
-    res.send('Server is up and running')
- })
+const url = `https://generativelanguage.googleapis.com/v1beta3/models/chat-bison-001:generateMessage?key=${KEY}`;
+
+app.listen(PORT, () => {
+  app.get("/", (req, res) => {
+    res.send('Server is up and running');
+  });
+
+  app.post("/query", async (req, res) => {
+    const query = req.body;
+
+    const payload = { 
+      prompt: { "messages": [{ "content": query.query }]},
+      temperature: 0.1,
+      candidate_count: 1
+    };
+
+    const sendQuery = async () => {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+      });
+      return response.json();
+    };
+
+    const data = await sendQuery();
+    res.send(data);
+  });
 });
-
-
--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,43 @@
 import "../src/style.css";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css"
+import { useState } from "react";
 const App = () => {
+
+    const [query, setQuery] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    console.log(messages);
+
+    const queryText = (e) => {
+        setQuery(e.target.value)
+    }
+
+    const askQuestionToAI = (e) => {
+        e.preventDefault();
+        if (query.length == 0) {
+            alert('Write Something First!')
+        } else {
+            fetch('http://localhost:5599/query', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query
+                })
+            })
+                .then(res => res.json())
+                .then(res => {
+                    setMessages([...messages, {
+                        author: res.messages[0].content,
+                        bot: res.candidates[0].content,
+                    }])
+                })
+                .catch(error => console.error('Error:', error));
+
+        }
+    }
+
     return (
         <div className="chat_section">
             <div className="chat_heading">
@@ -9,18 +46,24 @@ const App = () => {
             <div className="chatAreaContainer">
 
                 <div className="chat_history">
-                    <div className="reply_from_ai">
-                        <i className="fa-solid fa-robot"></i>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, velit.</p>
-                    </div>
-                    <div className="user_query">
-                        <i className="fa-regular fa-circle-user"></i>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, velit.</p>
-                    </div>
+                    {messages.map((message, index) => (
+                        <div className="message_block" key={index}>
+
+                            <div className="user_query">
+                                <i className="fa-regular fa-circle-user"></i>
+                                <p>{message.author}</p>
+                            </div>
+
+                            <div className="reply_from_ai">
+                                <i className="fa-solid fa-robot"></i>
+                                <p>{message.bot}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
                 <div className="send_message">
-                    <form>
-                        <input type="text" placeholder="Ask a question ..." />
+                    <form onSubmit={askQuestionToAI}>
+                        <input type="text" onChange={queryText} placeholder="Ask a question ..." />
                         <button><i className="fa-regular fa-paper-plane"></i></button>
                     </form>
                 </div>
